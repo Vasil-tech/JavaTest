5863ff5a6ff48dfe53d94f294af1fe0c2ba571a6
Vasil-tech <xamistic02@gmail.com>
2023-09-08 19:23:21 +0300

    debug

--- Classes/CAO.java
+++ Classes/CAO.java
@@ -3,19 +3,43 @@ package Classes;
 public class CAO {
     public static void main(String args[]){
         Person person1 = new Person();
-        person1.name = "Gus";
-        person1.age = 40;
+        // person1.name = "Gus";
+        // person1.age = 40;\
+        person1.setName("Gustavo");
+        person1.getName();
+        person1.setAge(25);
+        person1.getAge();
         person1.speak();
 
         Person person2 = new Person();
-        person2.name = "Alan";
-        person2.age = 25;
+        // person2.name = "Alan";
+        // person2.age = 25;
+        person1.setName("Georg");
+        person1.getName();
+        person2.setAge(40);
+        person2.getAge();
+        person2.speak();
+
     }
 }
 
 class Person{
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
 
     void speak(){
         System.out.println("My name is " + name + ", I'm " + age + " years old");
